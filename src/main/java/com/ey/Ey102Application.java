525fa32a06cfdee861740c5a3c0b36e92b204d28
ritit-gitdemo100 <riti.programs@gmail.com>
2026-01-07 12:22:01 +0530

    First Commit

---
+++ src/main/java/com/ey/Ey102Application.java
@@ -0,0 +1,13 @@
+package com.ey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ey102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ey102Application.class, args);
+	}
+
+}
